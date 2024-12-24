cf916d96ec286141ff9461687fa911d81cbaf0a4
Mohamed Alaa <ma0846924@gmail.com>
2024-12-24 03:03:15 +0200

    update

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,4 +1,4 @@
-export const baseURL = `http://127.0.0.1:8000/api`;
+export const baseURL = `https://e-commerce-laravel-production-bdef.up.railway.app/api`;
 //Auth
 export const REGISTER = "register";
 export const LOGIN = "login";
